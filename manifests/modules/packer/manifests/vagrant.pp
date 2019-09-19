1181a51d2559b69580e514b4c2f0b10814ddedc0
Matthew Radcliffe <mradcliffe@softpixel.com>
2019-09-19 13:19:25 -0400

    Fixes puppet errors

--- manifests/modules/packer/manifests/vagrant.pp
+++ manifests/modules/packer/manifests/vagrant.pp
@@ -4,7 +4,7 @@
 #
 class packer::vagrant(
   Optional[String] $home_base = $packer::vagrant::params::home_base,
-  Optional[String] $sudoers_file = $packer::vagrant::params::sudoer
+  Optional[String] $sudoers_file = $packer::vagrant::params::sudoers_file
 ) inherits packer::vagrant::params {
 
   group { 'vagrant':
@@ -32,10 +32,7 @@ class packer::vagrant(
   ssh_authorized_key { 'vagrant':
     ensure => present,
     user   => 'vagrant',
-    key    => 'AAAAB3NzaC1yc2EAAAABIwAAAQEA6NF8iallvQVp22WDkTkyrtvp9eWW6A8YVr+ \
-    kz4TjGYe7gHzIw+niNltGEFHzD8+v1I2YJ6oXevct1YeS0o9HZyN1Q9qgCgzUFtdOKLv6IedplqoPkcmF0aYet2PkEDo3MlTBckFXPITAM \
-    zF8dJSIFo9D8HfdOV0IAdx4O7PtixWKn5y2hMNG0zQPyUecp4pzC6kivAIhyfHilFR61RGL+ \
-    GPXQ2MWZWFYbAGjyiYJnAmCP3NOTd0jMZEnDkbUvxhMmBYSdETk1rRgm+R4LOzFUGaHqHDLKLX+FIPKcF96hrucXzcWyLbIbEgE98OHlnVYCzRdK8jlqm8tehUc9c9WhQ==',
+    key    => 'AAAAB3NzaC1yc2EAAAABIwAAAQEA6NF8iallvQVp22WDkTkyrtvp9eWW6A8YVr+kz4TjGYe7gHzIw+niNltGEFHzD8+v1I2YJ6oXevct1YeS0o9HZyN1Q9qgCgzUFtdOKLv6IedplqoPkcmF0aYet2PkEDo3MlTBckFXPITAMzF8dJSIFo9D8HfdOV0IAdx4O7PtixWKn5y2hMNG0zQPyUecp4pzC6kivAIhyfHilFR61RGL+GPXQ2MWZWFYbAGjyiYJnAmCP3NOTd0jMZEnDkbUvxhMmBYSdETk1rRgm+R4LOzFUGaHqHDLKLX+FIPKcF96hrucXzcWyLbIbEgE98OHlnVYCzRdK8jlqm8tehUc9c9WhQ==',
     type   => 'ssh-rsa',
   }
 
